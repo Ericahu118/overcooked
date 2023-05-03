44d99417751f305281c6bf1634bc48437c6d203a
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-03 15:04:08 +0800

    compile

--- source/main.cpp
+++ source/main.cpp
@@ -17,95 +17,6 @@ int worktype[2 + 5]; // 0 for dish 1 for wash
 int flag = 0;
 char Moveplan(double x, double y, int ptype)
 {
-    // 遵循先左右后上下 不能交互就要移动的策略
-    int line = ceil(Players[ptype].x + center) - 1;
-    int row = ceil(Players[ptype].y + center) - 1;
-    int line1 = ceil(Players[(ptype + 1) % 2].x + center) - 1;
-    int row1 = ceil(Players[(ptype + 1) % 2].y + center) - 1;
-    double dis = (Players[(ptype + 1) % 2].y - Players[ptype].y) * (Players[(ptype + 1) % 2].y - Players[ptype].y) + (Players[(ptype + 1) % 2].x - Players[ptype].x) * (Players[(ptype + 1) % 2].x - Players[ptype].x);
-    std::cerr << "player row: " << row << " line:" << line << std::endl;
-    std::cerr << "des row: " << x << " line:" << y << std::endl;
-
-    if (x < Players[ptype].x)
-    {
-        // 左
-        if (line > x + 1)
-        { // 不能交互
-            if (Map[row][line - 1] == '.' && (row != row1 || (line - 1) != line1))
-            { // 可走
-                return 'L';
-            }
-        }
-        else
-        {
-            return 'U';
-            if (y < Players[ptype].y)
-            {
-                // 上
-                if (row > y + 1)
-                { // 不能交互
-                    // std::cerr << "map:" << Map[row - 1][line] << std::endl;
-                    // std::cerr << "dis:" << dis<< std::endl;
-                    if (Map[row - 1][line] == '.' && dis > 0.5)
-                    {
-                        // 可走
-                        return 'U';
-                    }
-                }
-            }
-            else if (y > Players[ptype].y)
-            {
-                if (row < y - 1)
-                {
-                    // 不能交互
-                    if (Map[row + 1][line] == '.' && ((row + 1) != row1 || line != line1))
-                    {
-                        // 可走
-                        return 'D';
-                    }
-                }
-            }
-        }
-    }
-    else if (x > Players[ptype].x)
-    {
-        // 右
-        if (line < x - 1)
-        { // 不能交互
-            if (Map[row][line + 1] == '.' && (row != row1 || (line + 1) != line1))
-            { // 可走
-                return 'R';
-            }
-            else
-            {
-                if (y < Players[ptype].y)
-                {
-                    // 上
-                    if (row > y + 1)
-                    { // 不能交互
-                        if (Map[row - 1][line] == '.' && ((row - 1) != row1 || line != line1))
-                        {
-                            // 可走
-                            return 'U';
-                        }
-                    }
-                }
-                else if (y > Players[ptype].y)
-                {
-                    if (row < y - 1)
-                    {
-                        // 不能交互
-                        if (Map[row + 1][line] == '.' && ((row + 1) != row1 || line != line1))
-                        {
-                            // 可走
-                            return 'D';
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return 'N';
 }
 
 int main()
@@ -132,50 +43,10 @@ int main()
             continue;
 
         /* 输出当前帧的操作，此处仅作示例 */
-        std::cout << "Frame " << i << "\n";
-        char move0 = 'L';
-        char move1 = 'N';
-        std::string player0_Action;
-        std::string player1_Action;
-        switch (move0)
-        {
-        case 'U':
-            player0_Action = "Move U";
-            break;
-        case 'R':
-            player0_Action = "Move R";
-            break;
-        case 'D':
-            player0_Action = "Move D";
-            break;
-        case 'L':
-            player0_Action = "Move L";
-            break;
-        default:
-            player0_Action = "Move";
-            Players[0].X_Velocity = 0, Players[0].Y_Velocity = 0;
-            break;
-        }
-        switch (move1)
-        {
-        case 'U':
-            player1_Action = "Move U";
-            break;
-        case 'R':
-            player1_Action = "Move R";
-            break;
-        case 'D':
-            player1_Action = "Move D";
-            break;
-        case 'L':
-            player1_Action = "Move L";
-            break;
-        default:
-            player1_Action = "Move";
-            Players[1].X_Velocity = 0, Players[1].Y_Velocity = 0;
-            break;
-        }
-        std::cerr << "player x: " << Players[0].x << std::endl;
+
+        std::string player0_Action = "Move L";
+        std::string player1_Action = "Move";
+
         if (Players[0].x <= 2 && Players[0].entity.empty())
         {
             // flag = 1;
@@ -185,15 +56,8 @@ int main()
         {
             player0_Action = "Move";
         }
-        /* test
-        std::cerr << "player x: " << Players[0].x << "x" <<Ingredient[0].x  << std::endl;//可以输出到log.txt文件中
-        if(Players[0].x - Ingredient[0].x <= 2.3 && Players[0].entity.empty()){
-            player0_Action ="PutOrPick L";
-        }
-        if(!Players[0].entity.empty()){
-            player0_Action ="Move R";
-        }
-        test*/
+        if (Players[0].X_Velocity == 0)
+            Players[0].x = 1.5;
 
         /* 合成一个字符串再输出，否则输出有可能会被打断 */
         std::string action = player0_Action + "\n" + player1_Action + "\n";
