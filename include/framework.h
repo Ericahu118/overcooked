a58f3bac8a2454523a84f6cce2ccdbd0cf66a1df
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-04 13:39:21 +0800

    compile

--- include/framework.h
+++ include/framework.h
@@ -7,44 +7,43 @@
 
 struct Ingredient
 {
-    int x, y, price;//原料箱位置，原料价格
-    std::string name;//原料名称
+    int x, y, price;  // 原料箱位置，原料价格
+    std::string name; // 原料名称
 };
 
 struct Recipe
 {
-    int time;//加工时间
-    std::string nameBefore, nameAfter;//加工前name,加工后name
-    std::string kind;//加工类型
+    int time;                          // 加工时间
+    std::string nameBefore, nameAfter; // 加工前name,加工后name
+    std::string kind;                  // 加工类型
 };
 
 struct Order
 {
-    int validFrame;//有效时间
-    int price;//赚钱
-    int frequency;//权重
-    std::vector<std::string> recipe;//食材
+    int validFrame;                  // 有效时间
+    int price;                       // 赚钱
+    int frequency;                   // 权重
+    std::vector<std::string> recipe; // 食材
 };
 
 struct Player
 {
-    double x, y;//位置
+    double x, y; // 位置
     double X_Velocity;
     double Y_Velocity;
     int live;
-    ContainerKind containerKind;//手里实体
-    std::vector<std::string> entity;//实体数组？
+    ContainerKind containerKind;
+    std::vector<std::string> entity;
 };
 
 struct Entity
 {
-    double x, y;//坐标
-    ContainerKind containerKind;//实体容器
-    std::vector<std::string> entity;//？
+    double x, y;                     // 坐标
+    ContainerKind containerKind;     // 实体容器
+    std::vector<std::string> entity; // ？
     int currentFrame, totalFrame;
     int sum;
 };
-
 
 /* 初始化时的读入。 */
 void init_read();
