be36b307f6db93dcebc1ee3e20a26f327f5097b1
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-10 22:00:18 +0800

    compile

--- include/movement.h
+++ include/movement.h
@@ -8,7 +8,7 @@
 #include <string>
 #include <math.h>
 #include <framework.h>
-#include < destination.h>
+#include <destination.h>
 
 int changedir(double des_x, double des_y, int ptype);
 std::string frame_move(double des_x, double des_y, int ptype, int op);
