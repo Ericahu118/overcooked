460c03a626b175de4358c9a74ec4eb31176073bd
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-11 18:16:29 +0800

    compile

--- source/main.cpp
+++ source/main.cpp
@@ -93,7 +93,7 @@ int main()
 
         std::string player0_Action = "Move";
         std::string player1_Action = "Move";
-        /*string playerAction[2 + 5];
+        string playerAction[2 + 5];
         playerAction[0] = "Move";
         playerAction[1] = "Move";
         for (int ptype = 0; ptype <= 1; ptype++)
@@ -158,98 +158,98 @@ int main()
                     }
                 }
             }
-        }*/
-        if (Players[0].entity.empty())
-        {
-            for (int i = 0; i < IngredientCount; i++)
-            {
-                std::string need = Order[0].recipe[0];
-                if (Ingredient[i].name.compare(need) == 0)
-                {
-                    cerr << "1: step1" << endl;
-                    player0_Action = frame_move(Ingredient[i].x, Ingredient[i].y, 0, 0);
-
-                    break;
-                }
-            }
-            for (int i = 0; i < entityCount; i++)
-            {
-                if (Entity[i].containerKind == ContainerKind::Plate && !Entity[i].entity.empty())
-                {
-                    cerr << "1: step2" << endl;
-                    player0_Action = frame_move(Entity[i].x, Entity[i].y, 0, 0);
-                    break;
-                }
-            }
         }
-        else
-        {
-            if (Players[0].containerKind == ContainerKind::None)
-            {
-                for (int i = 0; i < entityCount; i++)
-                {
-                    if (Entity[i].containerKind == ContainerKind::Plate)
-                    {
-                        cerr << "1: step3" << endl;
-                        player0_Action = frame_move(Entity[i].x, Entity[i].y, 0, 0);
+        /*if (Players[0].entity.empty())
+         {
+             for (int i = 0; i < IngredientCount; i++)
+             {
+                 std::string need = Order[0].recipe[0];
+                 if (Ingredient[i].name.compare(need) == 0)
+                 {
+                     cerr << "1: step1" << endl;
+                     player0_Action = frame_move(Ingredient[i].x, Ingredient[i].y, 0, 0);
 
-                        break;
-                    }
-                }
-            }
-            else if (Players[0].containerKind == ContainerKind::Plate)
-            {
-                cerr << "1: step4" << endl;
-                player0_Action = frame_move(window.first, window.second, 0, 0);
-            }
-        }
+                     break;
+                 }
+             }
+             for (int i = 0; i < entityCount; i++)
+             {
+                 if (Entity[i].containerKind == ContainerKind::Plate && !Entity[i].entity.empty())
+                 {
+                     cerr << "1: step2" << endl;
+                     player0_Action = frame_move(Entity[i].x, Entity[i].y, 0, 0);
+                     break;
+                 }
+             }
+         }
+         else
+         {
+             if (Players[0].containerKind == ContainerKind::None)
+             {
+                 for (int i = 0; i < entityCount; i++)
+                 {
+                     if (Entity[i].containerKind == ContainerKind::Plate)
+                     {
+                         cerr << "1: step3" << endl;
+                         player0_Action = frame_move(Entity[i].x, Entity[i].y, 0, 0);
 
-        if (Players[1].containerKind == ContainerKind::DirtyPlates)
-        {
-            cerr << "2: step1" << endl;
-            player1_Action = frame_move(sink.first, sink.second, 1, 0);
-        }
-        else
-        {
-            bool flag = 0;
-            for (int i = 0; i < entityCount; i++)
-            {
-                if (Entity[i].containerKind == ContainerKind::DirtyPlates && Entity[i].x == sink.first && Entity[i].y == sink.second)
-                {
-                    cerr << "2: step2" << endl;
-                    flag = 1;
-                    break;
-                }
-            }
-            if (flag)
-                player1_Action = frame_move(sink.first, sink.second, 1, 1);
-            else
-            {
-                bool haveplate = false;
-                for (int i = 0; i < entityCount; i++)
-                {
-                    if (Entity[i].containerKind == ContainerKind::Plate)
-                    {
-                        haveplate = true;
-                    }
-                }
-                if (!haveplate)
-                {
+                         break;
+                     }
+                 }
+             }
+             else if (Players[0].containerKind == ContainerKind::Plate)
+             {
+                 cerr << "1: step4" << endl;
+                 player0_Action = frame_move(window.first, window.second, 0, 0);
+             }
+         }
 
-                    for (int i = 0; i < entityCount; i++)
-                    {
-                        if (Entity[i].containerKind == ContainerKind::DirtyPlates)
-                        {
-                            cerr << "2: step3" << endl;
-                            player1_Action = frame_move(Entity[i].x, Entity[i].y, 1, 0);
-                        }
-                    }
-                }
-            }
-        }
+         if (Players[1].containerKind == ContainerKind::DirtyPlates)
+         {
+             cerr << "2: step1" << endl;
+             player1_Action = frame_move(sink.first, sink.second, 1, 0);
+         }
+         else
+         {
+             bool flag = 0;
+             for (int i = 0; i < entityCount; i++)
+             {
+                 if (Entity[i].containerKind == ContainerKind::DirtyPlates && Entity[i].x == sink.first && Entity[i].y == sink.second)
+                 {
+                     cerr << "2: step2" << endl;
+                     flag = 1;
+                     break;
+                 }
+             }
+             if (flag)
+                 player1_Action = frame_move(sink.first, sink.second, 1, 1);
+             else
+             {
+                 bool haveplate = false;
+                 for (int i = 0; i < entityCount; i++)
+                 {
+                     if (Entity[i].containerKind == ContainerKind::Plate)
+                     {
+                         haveplate = true;
+                     }
+                 }
+                 if (!haveplate)
+                 {
+
+                     for (int i = 0; i < entityCount; i++)
+                     {
+                         if (Entity[i].containerKind == ContainerKind::DirtyPlates)
+                         {
+                             cerr << "2: step3" << endl;
+                             player1_Action = frame_move(Entity[i].x, Entity[i].y, 1, 0);
+                         }
+                     }
+                 }
+             }
+         }*/
         /* 合成一个字符串再输出，否则输出有可能会被打断 */
-        // std::string action = playerAction[0] + "\n" + playerAction[1] + "\n";
-        std::string action = player0_Action + "\n" + player1_Action + "\n";
+        std::string action = playerAction[0] + "\n" + playerAction[1] + "\n";
+        // std::string action = player0_Action + "\n" + player1_Action + "\n";
         std::cout << action;
 
         /* 不要忘记刷新输出流，否则游戏将无法及时收到响应 */
