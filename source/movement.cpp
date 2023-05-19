788e67e8c4a3a7607924161daef46283b84296a6
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-19 15:37:13 +0800

    compile

--- source/movement.cpp
+++ source/movement.cpp
@@ -37,10 +37,10 @@ int changedir(double des_x, double des_y, int ptype)
     v0x = Players[ptype].X_Velocity, v0y = Players[ptype].Y_Velocity;
     bool trapy = (y0 <= 8 && (8 - y0 <= center + 0.3)) && ((x0 < 8 && 8 - x0 <= center) || (x0 >= 8) && (x0 - 8 <= block + center));
     bool trapx = (x0 <= 8 && (8 - x0 <= center + 0.3) && ((y0 >= 8 && y0 - 8 <= block + center) || (y0 < 8 && 8 - y0 <= center)));
-    bool crashy1 = (y0 >= y1 && (y0 - y1 <= 2 * radius + 0.3)) && ((x0 < x1 && (x1 - x0) <= 2 * radius) || (x0 > x1) && (x0 - x1 <= 2 * radius));
-    bool crashy2 = (y0 <= y1 && (y1 - y0 <= 2 * radius + 0.3)) && ((x0 < x1 && (x1 - x0) <= 2 * radius) || (x0 > x1) && (x0 - x1 <= 2 * radius));
-    bool crashx1 = (x0 >= x1 && (x0 - x1 <= 2 * radius + 0.3)) && ((y0 < y1 && (y1 - y0) <= 2 * radius) || (y0 > y1) && (y0 - y1 <= 2 * radius));
-    bool crashx2 = (x0 < x1 && (x1 - x0 <= 2 * radius + 0.3)) && ((y0 < y1 && (y1 - y0) <= 2 * radius) || (y0 > y1) && (y0 - y1 <= 2 * radius));
+    bool crashy1 = (y0 >= y1 && (y0 - y1 <= 2 * radius + 0.5)) && ((x0 < x1 && (x1 - x0) <= 2 * radius) || (x0 > x1) && (x0 - x1 <= 2 * radius));
+    bool crashy2 = (y0 <= y1 && (y1 - y0 <= 2 * radius + 0.5)) && ((x0 < x1 && (x1 - x0) <= 2 * radius) || (x0 > x1) && (x0 - x1 <= 2 * radius));
+    bool crashx1 = (x0 >= x1 && (x0 - x1 <= 2 * radius + 0.5)) && ((y0 < y1 && (y1 - y0) <= 2 * radius) || (y0 > y1) && (y0 - y1 <= 2 * radius));
+    bool crashx2 = (x0 < x1 && (x1 - x0 <= 2 * radius + 0.5)) && ((y0 < y1 && (y1 - y0) <= 2 * radius) || (y0 > y1) && (y0 - y1 <= 2 * radius));
     bool getup = (y0 - des_y) <= block + center + 0.3;
     bool getdown = (des_y - y0) <= center + 0.3;
     bool getleft = (x0 - des_x) <= center + block + 0.3;
